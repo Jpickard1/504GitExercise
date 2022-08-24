774971dac0f6dd97f52b3d3be7ca216d813a7410
jpickard1 <jpic@umich.edu>
2022-08-24 10:11:00 -0400

    added doc string

--- code.py
+++ code.py
@@ -1,4 +1,7 @@
 def function1(a):
+    """
+    Function 1 doc string.
+    """
     b = dict()
     for c in a:
         if c not in b:
@@ -6,11 +9,14 @@ def function1(a):
         else:
             b[c] += 1
     return b
-
+      
 def function2(a):
+    """
+    Function 2 doc string.
+    """
     print('freqs')
     total = float(sum([a[b] for b in a.keys()]))
     for b in a.keys():
         print(b + ':' + str(a[b]/total))
-
+      
 function2(function1('ATCTGACGCGCGCCGC'))
